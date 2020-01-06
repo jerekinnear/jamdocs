aba5b87243ba24dc738de394c04ccccd61bf2c4e
jerekinnear <47419056+jerekinnear@users.noreply.github.com>
2020-01-06 09:36:38 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,9 +4,9 @@
       <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
-        <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/seo" text="SEO" icon="play-icon" />
+        <Shortcut link="/theme-configuration" text="CMS" icon="sliders-icon" />
+        <Shortcut link="/getting-started" text="Hébérgement" icon="eye-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
